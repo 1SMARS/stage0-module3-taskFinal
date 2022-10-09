83d986e109daac130875675966275ecddd7e5807
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-10 02:20:31 +0600

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,12 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        while (number > 0) {
-            System.out.println("true");
-            break;
-        }
-        while (number <= 0) {
-            System.out.println("false");
-            break;
+        System.out.println((number>0)?"true":"false");
+
     }
 }
